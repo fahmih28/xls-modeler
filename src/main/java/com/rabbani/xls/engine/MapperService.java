bc4babbe17c79a812730ede15360711aaa27b706
fulan <fahmih.rabbani@gmail.com>
2024-10-10 13:49:30 +0700

    create singleton sources

--- src/main/java/com/rabbani/xls/engine/MapperService.java
+++ src/main/java/com/rabbani/xls/engine/MapperService.java
@@ -3,9 +3,19 @@ package com.rabbani.xls.engine;
 import java.util.ServiceLoader;
 
 public class MapperService {
+
     private MapperFactory mapperFactory;
 
-    public MapperService() {
+    private static MapperService INSTANCE;
+
+    public static MapperService getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new MapperService();
+        }
+        return INSTANCE;
+    }
+
+    private MapperService() {
         mapperFactory = ServiceLoader.load(MapperFactory.class).iterator().next();
     }
 
